b7036057415d07ab9aff9aba1b790c1e349da3c6
awutang <779706234@qq.com>
2020-12-03 20:32:29 +0800

    add comment

--- src/com/phei/netty/aio/AcceptCompletionHandler.java
+++ src/com/phei/netty/aio/AcceptCompletionHandler.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013-2018 Lilinfeng.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,25 +20,31 @@ import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 
 /**
- * @author lilinfeng
- * @date 2014年2月16日
- * @version 1.0
+ * 对accept operation回调
  */
 public class AcceptCompletionHandler implements
-	CompletionHandler<AsynchronousSocketChannel, AsyncTimeServerHandler> {
+        CompletionHandler<AsynchronousSocketChannel, AsyncTimeServerHandler> {
 
+    /**
+     * 当accept operation成功后
+     * @param result
+     * @param attachment
+     */
     @Override
     public void completed(AsynchronousSocketChannel result,
-	    AsyncTimeServerHandler attachment) {
-	attachment.asynchronousServerSocketChannel.accept(attachment, this);
-	ByteBuffer buffer = ByteBuffer.allocate(1024);
-	result.read(buffer, buffer, new ReadCompletionHandler(result));
+                          AsyncTimeServerHandler attachment) {
+        // 接受新的客户端的连接，连接成功后回调completed（) 每当有一个客户端连接成功后，再异步接收新的客户端连接
+        // TODO:此处accept应该是开了一个线程future在异步等待accept吧，否则哪里体现异步性和回调呢？
+        attachment.asynchronousServerSocketChannel.accept(attachment, this);
+        ByteBuffer buffer = ByteBuffer.allocate(1024); // 分配1MB缓存即缓冲区
+        // 发起异步读，当读取成功后回调ReadCompletionHandler
+        result.read(buffer, buffer, new ReadCompletionHandler(result));
     }
 
     @Override
     public void failed(Throwable exc, AsyncTimeServerHandler attachment) {
-	exc.printStackTrace();
-	attachment.latch.countDown();
+        exc.printStackTrace();
+        attachment.latch.countDown(); // accept失败时减1，TODO：但是attachment实例只有一个，难道只要有一个accept失败就减1，然后服务端执行完程序结束吗？
     }
 
 }
--- src/com/phei/netty/nio/MultiplexerTimeServer.java
+++ src/com/phei/netty/nio/MultiplexerTimeServer.java
@@ -1,18 +1,7 @@
-/*
- * Copyright 2013-2018 Lilinfeng.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
+
+
+
 package com.phei.netty.nio;
 
 import java.io.IOException;
@@ -25,11 +14,6 @@ import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
-/**
- * @author Administrator
- * @date 2014年2月16日
- * @version 1.0
- */
 public class MultiplexerTimeServer implements Runnable {
 
     private Selector selector;
@@ -40,114 +24,122 @@ public class MultiplexerTimeServer implements Runnable {
 
     /**
      * 初始化多路复用器、绑定监听端口
-     * 
+     *
      * @param port
      */
     public MultiplexerTimeServer(int port) {
-	try {
-	    selector = Selector.open();
-	    servChannel = ServerSocketChannel.open();
-	    servChannel.configureBlocking(false);
-	    servChannel.socket().bind(new InetSocketAddress(port), 1024);
-	    servChannel.register(selector, SelectionKey.OP_ACCEPT);
-	    System.out.println("The time server is start in port : " + port);
-	} catch (IOException e) {
-	    e.printStackTrace();
-	    System.exit(1);
-	}
+        try {
+            selector = Selector.open(); // 创建selector
+            servChannel = ServerSocketChannel.open(); // 创建ServerSocketChannel
+            servChannel.configureBlocking(false); // 非阻塞
+            servChannel.socket().bind(new InetSocketAddress(port), 1024); // 绑定端口，类似BIO服务端
+			// 将ServerSocketChannel注册到selector，selector监听客户端的连接
+            servChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("The time server is start in port : " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     public void stop() {
-	this.stop = true;
+        this.stop = true;
     }
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see java.lang.Runnable#run()
      */
     @Override
     public void run() {
-	while (!stop) {
-	    try {
-		selector.select(1000);
-		Set<SelectionKey> selectedKeys = selector.selectedKeys();
-		Iterator<SelectionKey> it = selectedKeys.iterator();
-		SelectionKey key = null;
-		while (it.hasNext()) {
-		    key = it.next();
-		    it.remove();
-		    try {
-			handleInput(key);
-		    } catch (Exception e) {
-			if (key != null) {
-			    key.cancel();
-			    if (key.channel() != null)
-				key.channel().close();
-			}
-		    }
-		}
-	    } catch (Throwable t) {
-		t.printStackTrace();
-	    }
-	}
+        while (!stop) {
+            try {
+                selector.select(1000);
+				// 返回ready的channel,这个channel是客户端的还是服务器的？
+				// --从下面的代码看来，如果是已经连接了的则就是客户端channel,这也确实，本来就是监听客户端channel的read
+                // --TODO: 在源码层面是如何将SocketChannel与在服务端中创建的selector关联起来的？因为SocketChannel是注册到客户端selector的，估计是是通过ip port connect时做的
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectedKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+                    try {
+                        handleInput(key);
+                    } catch (Exception e) {
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null)
+                                key.channel().close();
+                        }
+                    }
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
 
-	// 多路复用器关闭后，所有注册在上面的Channel和Pipe等资源都会被自动去注册并关闭，所以不需要重复释放资源
-	if (selector != null)
-	    try {
-		selector.close();
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
+        // 多路复用器关闭后，所有注册在上面的Channel和Pipe等资源都会被自动去注册并关闭，所以不需要重复释放资源
+        if (selector != null)
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
 
     private void handleInput(SelectionKey key) throws IOException {
 
-	if (key.isValid()) {
-	    // 处理新接入的请求消息
-	    if (key.isAcceptable()) {
-		// Accept the new connection
-		ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
-		SocketChannel sc = ssc.accept();
-		sc.configureBlocking(false);
-		// Add the new connection to the selector
-		sc.register(selector, SelectionKey.OP_READ);
-	    }
-	    if (key.isReadable()) {
-		// Read the data
-		SocketChannel sc = (SocketChannel) key.channel();
-		ByteBuffer readBuffer = ByteBuffer.allocate(1024);
-		int readBytes = sc.read(readBuffer);
-		if (readBytes > 0) {
-		    readBuffer.flip();
-		    byte[] bytes = new byte[readBuffer.remaining()];
-		    readBuffer.get(bytes);
-		    String body = new String(bytes, "UTF-8");
-		    System.out.println("The time server receive order : "
-			    + body);
-		    String currentTime = "QUERY TIME ORDER"
-			    .equalsIgnoreCase(body) ? new java.util.Date(
-			    System.currentTimeMillis()).toString()
-			    : "BAD ORDER";
-		    doWrite(sc, currentTime);
-		} else if (readBytes < 0) {
-		    // 对端链路关闭
-		    key.cancel();
-		    sc.close();
-		} else
-		    ; // 读到0字节，忽略
-	    }
-	}
+        if (key.isValid()) {
+            // 处理新接入的请求消息
+            if (key.isAcceptable()) { // 根据operation bit 判断网络事件的类型
+                // Accept the new connection
+                ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                SocketChannel sc = ssc.accept(); // 三次握手建立连接、创建SocketChannel,TODO:这与在客户端中创建的SocketChannel有啥关系？
+                sc.configureBlocking(false);
+                // Add the new connection to the selector
+                sc.register(selector, SelectionKey.OP_READ);
+            }
+            if (key.isReadable()) {
+                // Read the data
+                SocketChannel sc = (SocketChannel) key.channel();
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024); // 因为无法预先知道客户端传输过来的数据大小，因此预先开辟1M的缓存
+                int readBytes = sc.read(readBuffer); // 非阻塞读 epoll_wait可以设置某一超时时间返回，因此不会被阻塞
+                if (readBytes > 0) {
+                    // 将limit设置为position,position设置为0，应该是之前read时将position写移动到了有效元素的下一个，
+                    // 因此如果之后要读取readBuffer的话需要将limit移到当前position处并position左移到0
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes, "UTF-8");
+                    System.out.println("The time server receive order : "
+                            + body);
+                    String currentTime = "QUERY TIME ORDER"
+                            .equalsIgnoreCase(body) ? new java.util.Date(
+                            System.currentTimeMillis()).toString()
+                            : "BAD ORDER";
+                    doWrite(sc, currentTime);
+                } else if (readBytes < 0) {
+                    // 对端链路关闭
+                    key.cancel();
+                    sc.close();
+                } else
+                    ; // 读到0字节，忽略
+            }
+        }
     }
 
     private void doWrite(SocketChannel channel, String response)
-	    throws IOException {
-	if (response != null && response.trim().length() > 0) {
-	    byte[] bytes = response.getBytes();
-	    ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
-	    writeBuffer.put(bytes);
-	    writeBuffer.flip();
-	    channel.write(writeBuffer);
-	}
+            throws IOException {
+        if (response != null && response.trim().length() > 0) {
+            byte[] bytes = response.getBytes();
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            // 此处position会移到最后write的下一个位置，因此需要flip()准备下次读取writeBuffer的数据将之写到channel中去
+            writeBuffer.put(bytes);
+            writeBuffer.flip();
+
+            channel.write(writeBuffer); // 可能会发生写半包
+        }
     }
 }
